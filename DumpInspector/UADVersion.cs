ceaf4aeadf5719ae2a8b926559f5ae99e5508f9a
Josh Stover <jmstover6@gmail.com>
2016-05-05 15:29:08 +1000

    Add VersionNumber back to UADVersion objects

--- DumpInspector/UADVersion.cs
+++ DumpInspector/UADVersion.cs
@@ -7,35 +7,46 @@ namespace ProjectRH {
 
     public class UADVersion {
 
+        public int VersionNumber      { get; private set; }
+
         public bool EncryptedPassword { get; private set; }
 
         public bool EncryptedUsername { get; private set; }
 
         public static readonly UADVersion Default = new UADVersion() {
+            VersionNumber = 0,
             EncryptedPassword = true,
             EncryptedUsername = false
         };
 
-        public static readonly IDictionary<int, UADVersion> KnownVersions = new Dictionary<int, UADVersion>() {
-            {6, new UADVersion() {
+        public static readonly IList<UADVersion> KnownVersions = new List<UADVersion>() {
+            Default,
+            new UADVersion() {
+                VersionNumber = 6,
                 EncryptedPassword = true,
                 EncryptedUsername = false,
-            }},
-
-            { 7, new UADVersion() {
+            },
+            new UADVersion() {
+                VersionNumber = 7,
                 EncryptedPassword = true,
                 EncryptedUsername = false,
-            }},
-
-            { 9, new UADVersion() {
+            },
+            new UADVersion() {
+                VersionNumber = 9,
                 EncryptedUsername = true,
                 EncryptedPassword = true
-            }}
+            }
         };
             
         public static UADVersion Get(int VersionNumber) {
-            UADVersion uad;
-            return KnownVersions.TryGetValue(VersionNumber, out uad) ? uad : UADVersion.Default ;
+            return KnownVersions.FirstOrDefault(u => u.VersionNumber == VersionNumber) ?? Default;
+        }
+
+        public override string ToString() {
+            if (VersionNumber == 0)
+                return "Custom";
+            else
+                return String.Format("UAD{0}", VersionNumber);
         }
 
     }
